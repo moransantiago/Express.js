c71919bc9059f386aa8e2e10019b60b4f523bd3a
Santiago Moran <santimoran.1@hotmail.com>
2019-09-11 17:28:05 -0300

    Second module finished: template engines and static files

---
+++ ecommerce/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const productsRouter = require('./routes/products');
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use('/products', productsRouter);
+
+const server = app.listen(3000, () => {
+    console.log(`Listening http://localhost:${server.address().port}`);
+});
